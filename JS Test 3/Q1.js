1f2b042c527262e512e14802d749a5547d140a8f
Musharaf1430 <alirazaali034121@gmail.com>
2025-02-22 22:08:27 +0500

    Add files via upload

---
+++ JS Test 3/Q1.js
@@ -0,0 +1,4 @@
+// 1. Declare a variable called age & assign to it your age. Show your age in an alert box.
+
+var age = 25;  
+alert("I am " + age + " years old");
---
+++ JS Test 3/Q2.js
@@ -0,0 +1,6 @@
+// 2. Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+var visitCount = 5;  // Yeh number update ho sakta hai
+alert("You have visited this site " + visitCount + " times");
