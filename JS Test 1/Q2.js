a5be5770cd40ccf2daef02d87a65584c7afb3eef
Musharaf1430 <alirazaali034121@gmail.com>
2025-02-22 21:05:51 +0500

    Add files via upload

---
+++ JS Test 1/Q2.js
@@ -0,0 +1,6 @@
+
+// 2. Write a script to display following message on your web page:
+alert("Error! Please enter a valid password.")
+
+
+
---
+++ JS Test 1/Q5.js
@@ -0,0 +1,2 @@
+// 5. Generate the following message through browser’s developer console:
+console.log("Hello! This is a message in the console.");
